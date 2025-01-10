f9fd86f0e950ad6eea1d55aa177d2c474e9bd020
Melbromss <melbromss@gmail.com>
2025-01-10 19:29:28 +0700

    change handler to handlers

--- server/internal/app/app.go
+++ server/internal/app/app.go
@@ -3,7 +3,7 @@ package app
 
 import (
 	"fmt"
-	"server/internal/api/handler"
+	"server/internal/api/handlers"
 	"server/internal/models"
 	"github.com/gofiber/fiber/v2"
 	"github.com/gofiber/fiber/v2/middleware/cors"
@@ -16,7 +16,7 @@ type App struct {
 	db      *gorm.DB
 	app     *fiber.App
 	config  *models.AppConfig
-	handler *handler.Handler
+	handlers *handlers.Handler
 }
 
 func NewApp(db *gorm.DB, cfg *models.AppConfig) *App {
@@ -35,13 +35,13 @@ func NewApp(db *gorm.DB, cfg *models.AppConfig) *App {
 	app.Use(logger.New())
 	app.Use(recover.New())
 
-	handler := handler.NewHandler(db)
+	handlers := handlers.NewHandler(db)
 
 	return &App{
 		app:     app,
 		db:      db,
 		config:  cfg,
-		handler: handler,
+		handlers: handlers,
 	}
 }
 
@@ -50,33 +50,33 @@ func (a *App) setupRoutes() {
 
 	// Buildings Routes
 	buildings := api.Group("/buildings")
-	buildings.Get("/", a.handler.GetListBuildings)
-	buildings.Get("/:id", a.handler.GetBuilding)
-	buildings.Post("/", a.handler.CreateBuilding)
-	buildings.Put("/:id", a.handler.UpdateBuilding)
-	buildings.Delete("/:id", a.handler.DeleteBuilding)
+	buildings.Get("/", a.handlers.GetListBuildings)
+	buildings.Get("/:id", a.handlers.GetBuilding)
+	buildings.Post("/", a.handlers.CreateBuilding)
+	buildings.Put("/:id", a.handlers.UpdateBuilding)
+	buildings.Delete("/:id", a.handlers.DeleteBuilding)
 
 	// Rooms Routes
 	rooms := api.Group("/rooms")
-	rooms.Get("/", a.handler.GetListRooms)
-	rooms.Get("/:id", a.handler.GetRoom)
-	rooms.Post("/", a.handler.CreateRoom)
-	rooms.Put("/:id", a.handler.UpdateRoom)
-	rooms.Delete("/:id", a.handler.DeleteRoom)
+	rooms.Get("/", a.handlers.GetListRooms)
+	rooms.Get("/:id", a.handlers.GetRoom)
+	rooms.Post("/", a.handlers.CreateRoom)
+	rooms.Put("/:id", a.handlers.UpdateRoom)
+	rooms.Delete("/:id", a.handlers.DeleteRoom)
 
 	// Item Routes
 	items := api.Group("/items")
-	items.Get("/", a.handler.GetListItems)
-	items.Get("/tables/:id", a.handler.GetTable)
-	items.Get("/toilets/:id", a.handler.GetToilet)
-	items.Post("/table", a.handler.CreateTable)
-	items.Post("/toilet", a.handler.CreateToilet)
-	items.Put("/available/:id", a.handler.UpdateItemAvailable)
+	items.Get("/", a.handlers.GetListItems)
+	items.Get("/tables/:id", a.handlers.GetTable)
+	items.Get("/toilets/:id", a.handlers.GetToilet)
+	items.Post("/table", a.handlers.CreateTable)
+	items.Post("/toilet", a.handlers.CreateToilet)
+	items.Put("/available/:id", a.handlers.UpdateItemAvailable)
 
 	// Booking Time Period Routes
 	bookingTimePeriods := api.Group("/booking-time-periods")
-	bookingTimePeriods.Get("/", a.handler.GetListBookingTimePeriods)
-	bookingTimePeriods.Get("/items", a.handler.GetBookingTimePeriodsByItemType)
+	bookingTimePeriods.Get("/", a.handlers.GetListBookingTimePeriods)
+	bookingTimePeriods.Get("/items", a.handlers.GetBookingTimePeriodsByItemType)
 
 	
 }
