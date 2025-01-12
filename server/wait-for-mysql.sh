565f04cce5612c4453c4bc450e24e885806a1483
Melbromss <melbromss@gmail.com>
2025-01-13 00:20:35 +0700

    edit compose

--- server/wait-for-mysql.sh
+++ server/wait-for-mysql.sh
@@ -18,12 +18,5 @@ for i in $(seq 1 30); do
   sleep 2
 done
 
-echo "Initializing database..."
-mysql -h"$host" -P"$port" -u"$user" -p"$password" --ssl=0 <<-EOSQL
-    ALTER USER 'root'@'%' IDENTIFIED BY 'Rsv18221';
-    GRANT ALL PRIVILEGES ON *.* TO 'root'@'%' WITH GRANT OPTION;
-    FLUSH PRIVILEGES;
-EOSQL
-
 echo "MySQL is up - executing command"
 exec "$@"
